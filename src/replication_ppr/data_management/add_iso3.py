f5d40114acd3237ff3d620c0b8b6d90d133c277b
Marcel Harald Wachter <marcelhwachter@outlook.com>
2023-03-12 16:04:12 +0100

    Add ISO3 Code to Countries List.

---
+++ src/replication_ppr/data_management/add_iso3.py
@@ -0,0 +1,7 @@
+import country_converter as coco
+import pandas as pd
+
+data = pd.read_csv("src/replication_ppr/data/countries_list.csv")
+countries_list = data["country"].tolist()
+standard_names = coco.convert(names=countries_list, to="ISO3")
+data["ISO3"] = standard_names
