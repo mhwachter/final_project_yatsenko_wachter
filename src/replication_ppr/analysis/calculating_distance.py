9a5077a82b0cad472651aadd5dcab9a57785a414
anzheyats <s6anyats@uni-bonn.de>
2023-03-14 20:25:12 +0100

    I implemented the CIA Factbook Data in the Distance calculation code

--- src/replication_ppr/analysis/calculating_distance.py
+++ src/replication_ppr/analysis/calculating_distance.py
@@ -15,44 +15,50 @@ def get_lat_long(country):
 
 
 # create dataframe
-df = pd.DataFrame(
-    {
-        "Country": ["Afghanistan", "Akrotiri"],
-        "Coordinates": ["47 20 N, 13 20 E", "41 00 N, 20 00 E"],
-    },
+
+distance_data = pd.read_csv(
+    "/Users/anzhelikayatsenko/Desktop/MASTERS/epp-2022/final_project/final_project_yatsenko_wachter/src/replication_ppr/data/cia_factbook.csv",
 )
 
+
 # create new columns for latitude and longitude
-df["Latitude"] = df["Country"].apply(
+
+distance_data["Latitude"] = distance_data["country"].apply(
     lambda x: get_lat_long(x)[0] if get_lat_long(x) is not None else None,
 )
-df["Longitude"] = df["Country"].apply(
+distance_data["Longitude"] = distance_data["country"].apply(
     lambda x: get_lat_long(x)[1] if get_lat_long(x) is not None else None,
 )
 
 # calculate distance between each pair of countries
 distances = []
 pair_id = 1
-for i in range(len(df)):
-    for j in range(i + 1, len(df)):
+for i in range(len(distance_data)):
+    for j in range(i + 1, len(distance_data)):
         if (
-            df["Latitude"][i] is not None
-            and df["Longitude"][i] is not None
-            and df["Latitude"][j] is not None
-            and df["Longitude"][j] is not None
+            distance_data["Latitude"][i] is not None
+            and distance_data["Longitude"][i] is not None
+            and distance_data["Latitude"][j] is not None
+            and distance_data["Longitude"][j] is not None
         ):
             d = distance(
-                (df["Latitude"][i], df["Longitude"][i]),
-                (df["Latitude"][j], df["Longitude"][j]),
+                (distance_data["Latitude"][i], distance_data["Longitude"][i]),
+                (distance_data["Latitude"][j], distance_data["Longitude"][j]),
             ).km
             pair_code = pair_id
             pair_id += 1
-            distances.append((df["Country"][i], df["Country"][j], pair_code, d))
+            distances.append(
+                (
+                    distance_data["country"][i],
+                    distance_data["country"][j],
+                    pair_code,
+                    d,
+                ),
+            )
 
 # create new dataframe with distance values
 distance_df = pd.DataFrame(
     distances,
     columns=["Country 1", "Country 2", "Pair ID", "Distance (km)"],
 )
-
 # print out dataframe
