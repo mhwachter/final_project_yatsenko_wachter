671af9fd7bd240855d9fb24906024855508a8470
Marcel Harald Wachter <marcelhwachter@outlook.com>
2023-03-17 20:16:43 +0100

    Cleaned up code.

--- src/replication_ppr/data_management/dot.py
+++ src/replication_ppr/data_management/dot.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 dot = pd.read_csv("../data/DOT_03-08-2023 16-25-54-79_panel.csv")
@@ -49,78 +48,4 @@ dot = (
 )
 dot.columns = [f"{a}{b}" for a, b in dot.columns]
 
-dot = dot[""]
-
-dot["c1_id"] = dot.groupby("Country Name").ngroup() + 1
-dot["c2_id"] = dot.groupby("Counterpart Country Name").ngroup() + 1
-
-m = dot["c1_id"].lt(dot["c2_id"])
-
-names = {
-    "c1_id": "c2_id",
-    "c2_id": "c1_id",
-    "Country Name": "Counterpart Country Name",
-    "Counterpart Country Name": "Country Name",
-    "FOB Exports": "FOB Exports 2",
-    "CIF Imports": "CIF Imports 2",
-}
-
-out = dot[m].merge(dot[~m].rename(columns=names), how="outer")
-
-dot["pair_id"] = (
-    dot.groupby(
-        dot[["Country Name", "Counterpart Country Name"]].apply(frozenset, axis=1),
-    ).ngroup()
-    + 1
-)
-
-
-dot["id"] = dot.groupby(["pair_id", "Time Period"]).ngroup()
-
-out_df = dot.set_index(["id", dot.groupby("id").cumcount()]).unstack()
-out_df.columns = [f"{a}{b}" for a, b in out_df.columns]
-
-
-dot["grp"] = dot.groupby(["Country Name", "Counterpart Country Name"]).ngroup()
-
-dot["subgrp"] = dot.groupby(["pair_id", "grp"]).ngroup()
-
-dot = dot.set_index(["pair_id", "subgrp"])
-
-dot.pivot_table(
-    index=["pair_id", "Time Period"],
-)
-
-dot["direction"] = (
-    dot.groupby(["Country Name", "Counterpart Country Name"]).ngroup() + 1
-)
-
-dot = dot.set_index(["pair_id", "direction"])
-
-dot = dot.set_index(["pair_id", "Time Period"])
-
-
-dot["subgroup"] = dot.groupby(["pair_id", "direction_id"]).ngroup()
-
-dot.groupby["pair_id", "direction_id"]
-
 dot.to_csv("/Users/marcel/Desktop/dot.csv")
-
-dot_wide = pd.pivot(
-    dot,
-    index=["pair_id", "Time Period"],
-    columns=["Country Name", "Counterpart Country Name"],
-    values=["FOB Exports", "CIF Imports"],
-)
-
-dot["countries"] = dot[["Country Name", "Counterpart Country Name"]].values.tolist()
-
-dot["combinedid"] = dot["Country Name"] + dot["Counterpart Country Name"]
-
-if (
-    dot["Country Name"] in dot["countries"]
-    and dot["Counterpart Country Name"] in dot["countries"]
-):
-    dot["pair_id"] = np.unique()
-
-pd.DataFrame(np.sort(dot.values, 1), index=dot.index).groupby([0, 1]).ngroup()
