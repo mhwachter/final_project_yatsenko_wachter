6cbba6804e12a1a052bd0724fee4882442c2cf28
Marcel Harald Wachter <marcelhwachter@outlook.com>
2023-03-21 11:44:52 +0100

    Worked on merging.

--- src/replication_ppr/data_management/final_dataset.py
+++ src/replication_ppr/data_management/final_dataset.py
@@ -4,15 +4,8 @@ dot = pd.read_csv("../../../bld/python/data/dot_final.csv")
 cia_dist = pd.read_csv("../../../bld/python/data/cia_distance.csv")
 rta = pd.read_csv("../../../bld/python/data/rta_final.csv")
 
-
-cia_dist = cia_dist.assign(
-    pair_id=list(map(frozenset, zip(cia_dist.ISO3_x, cia_dist.ISO3_y))),
-)
-
-cia_dist = cia_dist.reset_index(drop=True)
-
 comb = pd.merge(dot, rta, how="left", on="pair_year_id")
-comb = pd.merge(comb, cia_dist, how="left", left_on="pair_id_x", right_on="pair_id_x")
+comb = pd.merge(comb, cia_dist, how="left", left_on="pair_id_y", right_on="pair_id")
 
 
 comb = comb[
