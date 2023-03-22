000eb94c19ff281a3b5444f23864d8e8a36fa452
anzheyats <s6anyats@uni-bonn.de>
2023-03-22 21:56:18 +0100

    I created a py file that creates a Dataset for countries that Joined WTO/GATT

--- src/replication_ppr/analysis/WTO_data.py
+++ src/replication_ppr/analysis/WTO_data.py
@@ -1,10 +1,30 @@
 import pandas as pd
 
-# WTO countries code to delete to date and just leave the year.
-join = pd.read_excel("src/replication_ppr/data/Countries_member_WTO.xls")
-join = pd.DataFrame(join)
-join = join.rename(columns={"Membership Date": "Membership_Date"})
-join["Membership_Date"] = pd.to_datetime(join["Membership_Date"])
-join["join"] = join["Membership_Date"].dt.strftime("%Y")
-join["join"] = join["join"].fillna(10000)
-join = join.drop(columns="Membership_Date")
+dist = pd.read_csv("../../../bld/python/data/distance_final.csv")
+# Work with WTO data.
+WTO = pd.read_excel(
+    "/Users/anzhelikayatsenko/Desktop/MASTERS/epp-2022/final_project/final_project_yatsenko_wachter/src/replication_ppr/data/WTO_members.xlsx",
+    sheet_name="mem-obs-list",
+)
+WTO = pd.DataFrame(WTO)
+WTO["Year"] = pd.to_datetime(WTO["Membership Date"]).dt.year
+WTO["Year"] = WTO["Year"].fillna("5000")
+WTO = WTO.drop("Membership Date", axis=1)
+# GATT Data
+GATT = pd.read_excel(
+    "/Users/anzhelikayatsenko/Desktop/MASTERS/epp-2022/final_project/final_project_yatsenko_wachter/src/replication_ppr/data/GATT.xlsx",
+    sheet_name="mem-obs-list",
+)
+GATT = pd.DataFrame(GATT)
+GATT["Year"] = pd.to_datetime(GATT["Membership Date"]).dt.year
+GATT = GATT.drop("Membership Date", axis=1)
+# Merging
+GATT_WTO = pd.merge(GATT, WTO, how="outer", on="Members")
+new_names = {"Year_x": "GATT", "Year_y": "WTO"}
+GATT_WTO = GATT_WTO.rename(columns=new_names)
+GATT_WTO["GATT"] = GATT_WTO["GATT"].fillna(GATT_WTO["WTO"])
+GATT_WTO = pd.DataFrame(GATT_WTO)
+GATT_WTO["join"] = GATT_WTO[["GATT", "WTO"]].min(axis=1)
+GATT_WTO = GATT_WTO.drop(["GATT", "WTO"], axis=1)
+#
+pairs = dist.loc[:, ["pair_id", "ctry1", "ctry2"]]
