f1899450fe79f6e0f0d1c6f91e394abe289a6ee8
Marcel Harald Wachter <marcelhwachter@outlook.com>
2023-03-29 23:57:34 +0200

    Created tables for regressions on our own data.

---
+++ src/replication_ppr/analysis/task_regressions_created_data.py
@@ -0,0 +1,52 @@
+import pytask
+
+from replication_ppr.config import BLD, SRC
+
+
+@pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+@pytask.mark.r(script=SRC / "analysis" / "tab1_models.r")
+@pytask.mark.produces(BLD / "R" / "models" / "tab1_models_created_data.rds")
+def task_analysis_r1():
+    pass
+
+
+@pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+@pytask.mark.r(script=SRC / "analysis" / "tab2_models.r")
+@pytask.mark.produces(BLD / "R" / "models" / "tab2_models_created_data.rds")
+def task_analysis_r2():
+    pass
+
+
+@pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+@pytask.mark.r(script=SRC / "analysis" / "tab3_models.r")
+@pytask.mark.produces(BLD / "R" / "models" / "tab3_models_created_data.rds")
+def task_analysis_r3():
+    pass
+
+
+@pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+@pytask.mark.r(script=SRC / "analysis" / "tab4_models.r")
+@pytask.mark.produces(BLD / "R" / "models" / "tab4_models_created_data.rds")
+def task_analysis_r4():
+    pass
+
+
+# @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+# @pytask.mark.r(script=SRC / "analysis" / "tab5_models.r")
+# @pytask.mark.produces(BLD / "R" / "models" / "tab5_models_created_data.rds")
+# def task_analysis_r5():
+#     pass
+
+
+# @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+# @pytask.mark.r(script=SRC / "analysis" / "tab6_models.r")
+# @pytask.mark.produces(BLD / "R" / "models" / "tab6_models_created_data.rds")
+# def task_analysis_r6():
+#     pass
+
+
+@pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+@pytask.mark.r(script=SRC / "analysis" / "tab8_models.r")
+@pytask.mark.produces(BLD / "R" / "models" / "tab8_models_created_data.rds")
+def task_analysis_r8():
+    pass
---
+++ src/replication_ppr/final/task_final_created_data.py
@@ -0,0 +1,52 @@
+import pytask
+
+from replication_ppr.config import BLD, SRC
+
+
+@pytask.mark.depends_on(BLD / "R" / "models" / "tab1_models_created_data.rds")
+@pytask.mark.r(script=SRC / "final" / "table1_results.r")
+@pytask.mark.produces(BLD / "R" / "tables" / "tab1_results_created_data.tex")
+def task_final_r1_own():
+    pass
+
+
+@pytask.mark.depends_on(BLD / "R" / "models" / "tab2_models_created_data.rds")
+@pytask.mark.r(script=SRC / "final" / "table2_results.r")
+@pytask.mark.produces(BLD / "R" / "tables" / "tab2_results_created_data.tex")
+def task_final_r2_own():
+    pass
+
+
+@pytask.mark.depends_on(BLD / "R" / "models" / "tab3_models_created_data.rds")
+@pytask.mark.r(script=SRC / "final" / "table3_results.r")
+@pytask.mark.produces(BLD / "R" / "tables" / "tab3_results_created_data.tex")
+def task_final_r3_own():
+    pass
+
+
+@pytask.mark.depends_on(BLD / "R" / "models" / "tab4_models_created_data.rds")
+@pytask.mark.r(script=SRC / "final" / "table4_results.r")
+@pytask.mark.produces(BLD / "R" / "tables" / "tab4_results_created_data.tex")
+def task_final_r4_own():
+    pass
+
+
+# @pytask.mark.depends_on(BLD / "R" / "models" / "tab5_models_created_data.rds")
+# @pytask.mark.r(script=SRC / "final" / "table5_results.r")
+# @pytask.mark.produces(BLD / "R" / "tables" / "tab5_results_created_data.tex")
+# def task_final_r5_own():
+#     pass
+
+
+# @pytask.mark.depends_on(BLD / "R" / "models" / "tab6_models_created_data.rds")
+# @pytask.mark.r(script=SRC / "final" / "table6_results.r")
+# @pytask.mark.produces(BLD / "R" / "tables" / "tab6_results_created_data.tex")
+# def task_final_r6_own():
+#     pass
+
+
+@pytask.mark.depends_on(BLD / "R" / "models" / "tab8_models_created_data.rds")
+@pytask.mark.r(script=SRC / "final" / "table8_results.r")
+@pytask.mark.produces(BLD / "R" / "tables" / "tab8_results_created_data.tex")
+def task_final_r8_own():
+    pass
