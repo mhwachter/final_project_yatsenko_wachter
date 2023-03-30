dc97ecc26473c79f6732efcc151f157d0cdb2ff4
Marcel Harald Wachter <marcelhwachter@outlook.com>
2023-03-30 14:29:38 +0200

    Fixed various issues with regressions.

--- src/replication_ppr/analysis/task_regressions_created_data.py
+++ src/replication_ppr/analysis/task_regressions_created_data.py
@@ -7,47 +7,47 @@ from replication_ppr.config import BLD, SRC
 @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
 @pytask.mark.r(script=SRC / "analysis" / "tab1_models.r")
 @pytask.mark.produces(BLD / "R" / "models" / "tab1_models_created_data.rds")
-def task_analysis_r1():
+def task_analysis_r1_own():
     pass
 
 
 @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
 @pytask.mark.r(script=SRC / "analysis" / "tab2_models.r")
 @pytask.mark.produces(BLD / "R" / "models" / "tab2_models_created_data.rds")
-def task_analysis_r2():
+def task_analysis_r2_own():
     pass
 
 
 @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
 @pytask.mark.r(script=SRC / "analysis" / "tab3_models.r")
 @pytask.mark.produces(BLD / "R" / "models" / "tab3_models_created_data.rds")
-def task_analysis_r3():
+def task_analysis_r3_own():
     pass
 
 
 @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
 @pytask.mark.r(script=SRC / "analysis" / "tab4_models.r")
 @pytask.mark.produces(BLD / "R" / "models" / "tab4_models_created_data.rds")
-def task_analysis_r4():
+def task_analysis_r4_own():
     pass
 
 
-# @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
-# @pytask.mark.r(script=SRC / "analysis" / "tab5_models.r")
-# @pytask.mark.produces(BLD / "R" / "models" / "tab5_models_created_data.rds")
-# def task_analysis_r5():
-#     pass
+@pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
+@pytask.mark.r(script=SRC / "analysis" / "tab5_models_excl_JPN.r")
+@pytask.mark.produces(BLD / "R" / "models" / "tab5_models_created_data.rds")
+def task_analysis_r5_own():
+    pass
 
 
 # @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
 # @pytask.mark.r(script=SRC / "analysis" / "tab6_models.r")
 # @pytask.mark.produces(BLD / "R" / "models" / "tab6_models_created_data.rds")
-# def task_analysis_r6():
+# def task_analysis_r6_own():
 #     pass
 
 
 @pytask.mark.depends_on(BLD / "python" / "data" / "data_final.csv")
 @pytask.mark.r(script=SRC / "analysis" / "tab8_models.r")
 @pytask.mark.produces(BLD / "R" / "models" / "tab8_models_created_data.rds")
-def task_analysis_r8():
+def task_analysis_r8_own():
     pass
