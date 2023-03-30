5900041521568720a77fffb2f9b0e4b6e5f6b5b8
Marcel Harald Wachter <marcelhwachter@outlook.com>
2023-03-30 17:35:50 +0200

    Cleaned up data management folder.

--- src/replication_ppr/data_management/add_iso3.py
+++
@@ -1,9 +0,0 @@
-"Python script to create one standard for various country names across dataset."
-import country_converter as coco
-import pandas as pd
-
-data = pd.read_csv("src/replication_ppr/data/countries_list.csv")
-countries_list = data["country"].tolist()
-standard_names = coco.convert(names=countries_list, to="ISO3")
-data["ISO3"] = standard_names
-data.to_csv("src/replication_ppr/data/countries_list.csv")
--- src/replication_ppr/data_management/original_data_paper.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-original_data_paper = pd.read_csv("../data/original_data_paper.csv")
